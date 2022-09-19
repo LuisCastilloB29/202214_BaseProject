002016d6da3d6b72d9051ce2c42a5c8906983c62
Luis Castillo <l.castillo1@uniandes.edu.co>
2022-09-18 21:37:45 -0500

    Add general modifications

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SupermercadoModule } from './supermercado/supermercado.module';
+import { CiudadModule } from './ciudad/ciudad.module';
+import { CiudadSupermercadoController } from './ciudad-supermercado/ciudad-supermercado.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CiudadEntity } from "./ciudad/ciudad.entity";
+import { SupermercadoEntity } from "./supermercado/supermercado.entity";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    SupermercadoModule,
+    CiudadModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '1234',
+      database: 'supermercados',
+      entities: [
+        CiudadEntity,
+        SupermercadoEntity,
+      ],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true,
+  }),],
+  controllers: [AppController, CiudadSupermercadoController],
   providers: [AppService],
 })
 export class AppModule {}
